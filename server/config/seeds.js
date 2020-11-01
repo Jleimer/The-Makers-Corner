016ac72aa51f5b6ed3469ecd4804bf76f35412ad
Jonathan <eimerarts@gmail.com>
2020-10-31 21:01:05 -0500

    working on seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -3,7 +3,7 @@ const { User, Class, Blueprint } = require('../models')
 
 await Class.deleteMany();
 
-const classes = await Class. insertMany([
+const classes = await Class.insertMany([
     {
        name: 'Resin Pouring',
        description: 'Learn how to do the trending resin pouring technique from your home! We will ship you the supplies and you will attend the scheduled Zoom class with our specialized instructor.  Learn to make your very own resin and wood cutting board or decorative piece.',
@@ -19,4 +19,47 @@ const classes = await Class. insertMany([
         difficulty: 'Beginner',
         items: 'Soldering Iron, flux, glass foil, precut stained glass, glass cutter'
     }
-])
+]);
+
+await Blueprint.deleteMany();
+
+const bluePrint = await Blueprint.insertMany([
+    {
+      name: 'Workbench',
+      description: 'This is an easy work that can fit in almost any garage or shop. Storage underneath and a places for a small tools on top. You can easliy add wheels to this for the perfect mobile bench.',
+      image: '',
+      file: '',
+      price: 6.00,
+      difficulty: 'Some exprience required' 
+    },
+    {
+      name: 'Shaker cabinet door',
+      description: 'Plans for basic shaker cabinet doors.  Just adjust plans for custom measurements and you are set!',
+      image: '',
+      file: {},
+      price: 5.00,
+      difficulty: 'Experience with table saw required'
+    }
+]);
+
+await User.deleteMany();
+
+  await User.create({
+    firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id]
+      }
+    ]
+  });
+
+  await User.create({
+    firstName: 'Elijah',
+    lastName: 'Holt',
+    email: 'eholt@testmail.com',
+    password: 'password12345'
+  });
+
