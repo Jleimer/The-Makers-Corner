0bfa2e00f76549232cb1845ea975ba0b6aaeef30
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-11-01 12:29:25 -0600

    seeding properly for all. need to add posts

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,69 +1,94 @@
-const db = require('./connection');
-const { User, Class, Blueprint } = require('../models')
+const db = require("./connection");
+const { User, Class, Blueprint, Category, Order, Post } = require("../models");
 
-db.once('open', async () => {
-await Class.deleteMany();
+db.once("open", async () => {
+  await Category.deleteMany();
 
-const classes = await Class.insertMany([
+  const categories = await Category.insertMany([
+    { name: "metal working" },
+    { name: "wood working" },
+    { name: "digital" },
+    { name: "print working" },
+    { name: "textile" },
+    { name: "ceramics" },
+  ]);
+
+  console.log("categories seeded");
+
+  await User.deleteMany();
+
+  await User.create(
     {
-       name: 'Resin Pouring',
-       description: 'Learn how to do the trending resin pouring technique from your home! We will ship you the supplies and you will attend the scheduled Zoom class with our specialized instructor.  Learn to make your very own resin and wood cutting board or decorative piece.',
-       price: 85.00,
-       difficulty: 'Beginner',
-       items: 'Resin, wood(walnut), pouring frame'
-
+      firstName: "Jonathan",
+      lastName: "Eimer",
+      email: "jleimer@testmail.com",
+      username: "jleimer",
+      password: "ghost",
     },
     {
-        name: 'Stained Glass',
-        description: 'Want to learn stained Glass!? Here is the place to do it. We will send you a box with all the tools and supplies you need to make our beginner stained glass piece that you can hang in your window. Attend the scheduled Zoom meeting with our spedcialized instructor',
-        price: 80.00,
-        difficulty: 'Beginner',
-        items: 'Soldering Iron, flux, glass foil, precut stained glass, glass cutter'
+      firstName: "Keegan",
+      lastName: "Wedwick",
+      email: "kwedwick@testmail.com",
+      password: "ghost",
+      username: "kwedwick",
     }
-]);
+  );
 
-await Blueprint.deleteMany();
+  console.log("User(s) seeded");
 
-const bluePrint = await Blueprint.insertMany([
+  await Class.deleteMany();
+  const classes = await Class.insertMany([
     {
-      name: 'Workbench',
-      description: 'This is an easy work that can fit in almost any garage or shop. Storage underneath and a places for a small tools on top. You can easliy add wheels to this for the perfect mobile bench.',
-      image: '',
-      file: '',
-      price: 6.00,
-      difficulty: 'Some exprience required' 
+      name: "Resin Pouring",
+      description:
+        "Learn how to do the trending resin pouring technique from your home! We will ship you the supplies and you will attend the scheduled Zoom class with our specialized instructor.  Learn to make your very own resin and wood cutting board or decorative piece.",
+      price: 85.0,
+      difficulty: "Beginner",
+      items: "Resin, wood(walnut), pouring frame",
+      category: categories[2]._id,
+      username: "jleimer"
     },
     {
-      name: 'Shaker cabinet door',
-      description: 'Plans for basic shaker cabinet doors.  Just adjust plans for custom measurements and you are set!',
-      image: '',
-      file: {},
-      price: 5.00,
-      difficulty: 'Experience with table saw required'
-    }
-]);
+      name: "Stained Glass",
+      description:
+        "Want to learn stained Glass!? Here is the place to do it. We will send you a box with all the tools and supplies you need to make our beginner stained glass piece that you can hang in your window. Attend the scheduled Zoom meeting with our spedcialized instructor",
+      price: 80.0,
+      difficulty: "Beginner",
+      items:
+        "Soldering Iron, flux, glass foil, precut stained glass, glass cutter",
+      category: categories[0]._id,
+      username: "kwedwick"
+    },
+  ]);
+  console.log("classes seeded");
 
-await User.deleteMany();
+  await Blueprint.deleteMany();
 
-  await User.create({
-    firstName: 'Jonathan',
-    lastName: 'Eimer',
-    email: 'jleimer@testmail.com',
-    username: 'jleimer',
-    password: 'ghost',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
-  });
+  const blueprint = await Blueprint.insertMany([
+    {
+      name: "Workbench",
+      description:
+        "This is an easy work that can fit in almost any garage or shop. Storage underneath and a places for a small tools on top. You can easliy add wheels to this for the perfect mobile bench.",
+      image: "asdfsadf",
+      file: "asdf",
+      price: 6.0,
+      difficulty: "Some exprience required",
+      category: categories[3]._id,
+      username: "jleimer"
+    },
+    {
+      name: "Shaker cabinet door",
+      description:
+        "Plans for basic shaker cabinet doors.  Just adjust plans for custom measurements and you are set!",
+      image: "asdfasdf",
+      file: "asdfasdf",
+      price: 5.0,
+      difficulty: "Experience with table saw required",
+      category: categories[4]._id,
+      username: "kwedwick"
+    },
+  ]);
+  console.log("blueprints seeded");
 
-  await User.create({
-    firstName: 'Keegan',
-    lastName: 'Wedwick',
-    email: 'kwedwick@testmail.com',
-    password: 'ghost'
-  });
-  
   process.exit();
 });
