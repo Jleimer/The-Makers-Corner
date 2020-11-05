903676618d986dccc323091da8eeef5ca5113af3
alruet <rueter.alison@gmail.com>
2020-11-04 15:39:40 -0900

    uninstalled materialize

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -4,6 +4,27 @@ const Dashboard = () => {
     return (
         <div>
             <h2>Dashboard</h2>
+            <div>
+                <h3>Add a Course</h3>
+                <div className="form-div">
+                    <form>
+                        <div>
+                            <label htmlFor="name">Course Name: </label>
+                            <input
+                                className="input"
+                                placeholder="johndoe@test.com"
+                                name="email"
+                                type="email"
+                                id="email"
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div>
+                <h3>Add a Blueprint</h3>
+            </div>
         </div>
     );
 };
