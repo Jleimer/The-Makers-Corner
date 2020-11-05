81b9e05e4c245331af8e1314e3fad39d8406e94c
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-11-05 10:45:09 -0600

    updated actions

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,3 +1,20 @@
+//AFFECTS STATE
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const ADD_USER = 'ADD_USER';
+export const LOGIN = 'LOGIN';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const ADD_POST = 'ADD_POST';
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART"
+export const TOGGLE_CART = "TOGGLE_CART";
+export const UPDATE_BLUEPRINTS = "UPDATE_BLUEPRINTS";
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+export const ADD_COURSES = "ADD_COURSES";
+export const UPDATE_COURSES = "UPDATE_COURSES";
+
+
+
+//old actions
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const ADD_USER = 'ADD_USER';
 export const LOGIN = 'LOGIN';
